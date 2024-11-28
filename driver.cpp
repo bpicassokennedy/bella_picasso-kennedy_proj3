dbdc882cd2330595454e7903def8479595c6598e
bpicassokennedy <b3llakennedy@gmail.com>
2024-11-28 08:14:37 +0000

    okay

--- driver.cpp
+++ driver.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 #include <string> // defintion for the string class 
+#include <sstream>
 #include <fstream> // reading input from the txt file
 #include <cstdint> // provides fixed width integer types (ex. 32-bit unsigned integer)
 #include <iomanip> // input + output manipulator, control hexademical formatting 
 
-void readData(std::string arr[], int max);
+int readData(std::string arr[], int numInstructions);
 void display(std::string instructions[], uint32_t registers[], bool update, int max);
-void parseInstruction(std::string instruction, uint32_t registers[], std::string instructions[]);
+void parseInstruction(std::string instruction, uint32_t registers[], std::string instructions[], bool& update);
 void executeInstruction(std::string opcode, int rd, int rn, int rm, std::string immediateStr, uint32_t registers[], bool update, std::string instructions[]);
-//int parseImmediate(std::string line);
+uint32_t parseImmediate(std::string line);
 
 int main(){
     static const int maxCount = 50;
     std::string instructions[maxCount]; // stores each line of instruction 
     
-    readData(instructions, maxCount);
+    int numInstructions = readData(instructions, maxCount); 
   
     static const int numRegisters = 8;
     uint32_t registers[numRegisters] = {0}; // initialize all registers to 0
-    
+
     bool update = false;
-    display(instructions, registers, update, maxCount);
+    display(instructions, registers, update, numInstructions); // numInstructions used as looping condition in following functions
 
     return 0; // code executed successfully! 
 }
 
-void readData(std::string arr[], int max){ // want to modify the array so not passing through const
+int readData(std::string arr[], int max){ // want to modify the array so not passing through const
     std::ifstream inFile("programming-project-3.txt");
     if(!inFile){
         std::cout << "error opening file!" << std::endl;
@@ -38,21 +39,19 @@ void readData(std::string arr[], int max){ // want to modify the array so not pa
         count++;
     }
     inFile.close();
+    return count;
 }
 
-void display(std::string instructions[], uint32_t registers[], bool update, int max){
-    for(int i = 0; i < max; i++){
+void display(std::string instructions[], uint32_t registers[], bool update, int numInstructions){
+    for(int i = 0; i < numInstructions; i++){
         std::cout << instructions[i] << std::endl;
-        parseInstruction(instructions[i], registers, instructions);
+        parseInstruction(instructions[i], registers, instructions, update);  
+        std::cout << "R0: 0x" << std::hex << std::uppercase << registers[0] << " R1: 0x" << std::hex << std::uppercase << registers[1] << " R2: 0x" << std::hex << std::uppercase << registers[2] << " R3: 0x" << std::hex << std::uppercase << registers[3] << std::endl;
+        std::cout << "R4: 0x" << std::hex << std::uppercase << registers[4] << " R5: 0x" << std::hex << std::uppercase << registers[5] << " R6: 0x" << std::hex << std::uppercase << registers[6] << " R7: 0x" << std::hex << std::uppercase << registers[7] << std::endl << std::endl;
     }
-    for(int i = 0; i < 8; i++){
-        std::cout << "R0: " << registers[0] << " R1: " << registers[1] << " R2: " << registers[2] << " R3: " << registers[3] << std::endl;
-        std::cout << "R4: " << registers[4] << " R5: " << registers[5] << " R6: " << registers[6] << " R7: " << registers[7] << std::endl;
-    }
-    //updateFlags();
 }
 
-void parseInstruction(std::string instruction, uint32_t registers[], std::string instructions[]){
+void parseInstruction(std::string instruction, uint32_t registers[], std::string instructions[], bool& update){
     std::string opcode, rd, rn, rm, immediate;
     std::stringstream ss(instruction);
     bool updateFlags = false; // are we updating instructions or preserving from previous instruction?
@@ -66,10 +65,8 @@ void parseInstruction(std::string instruction, uint32_t registers[], std::string
 
     if(opcode == "MOV"){
         ss >> rd >> immediate; // moving immediate into rd 
-
         char num = rd[1];
         int rdNum = num - '0';
-
         executeInstruction(opcode, rdNum, 0, 0, immediate, registers, updateFlags, instructions);
     }
     else if(opcode == "LSLS" || opcode == "LSRS"){
@@ -77,12 +74,10 @@ void parseInstruction(std::string instruction, uint32_t registers[], std::string
         if(opcode.back() == 'S'){
             updateFlags = true;
         }
-
         char num1 = rd[1];
         int rdNum = num1 - '0';
         char num2 = rn[1];
         int rnNum = num2 - '0';
-
         executeInstruction(opcode, rdNum, rnNum, 0, immediate, registers, updateFlags, instructions);
     }
     else if(opcode == "ADDS" || opcode == "SUBS" || opcode == "ANDS" || opcode == "ORR" || opcode == "XOR"){
@@ -90,14 +85,12 @@ void parseInstruction(std::string instruction, uint32_t registers[], std::string
         if(opcode.back() == 'S'){
             updateFlags = true; 
         }
-
         char num1 = rd[1];
         int rdNum = num1 - '0';
         char num2 = rn[1];
         int rnNum = num2 - '0';
         char num3 = rm[1];
         int rmNum = num3 - '0';
-
         executeInstruction(opcode, rdNum, rnNum, rmNum, "", registers, updateFlags, instructions);
     }
     else{
@@ -108,10 +101,8 @@ void parseInstruction(std::string instruction, uint32_t registers[], std::string
 void executeInstruction(std::string opcode, int rd, int rn, int rm, std::string immediateStr, uint32_t registers[], bool update, std::string instructions[]){
     int immediateValue; 
     if(opcode == "MOV"){
-        // immediateValue = parseImmediate(immediateStr);
-        // std::cout << immediateValue << std::endl;
-        // registers[rd] = immediateValue; 
-       //std::cout << registers[rd] << std::endl;
+        uint32_t movValue = parseImmediate(immediateStr);
+        registers[rd] = movValue; 
     }
     else if(opcode == "LSLS" || opcode == "LSRS"){
         if(opcode == "LSLS"){
@@ -146,21 +137,19 @@ void executeInstruction(std::string opcode, int rd, int rn, int rm, std::string 
         }
     }
     else{
-        std::cout << "not supported!" << std::endl;
+        std::cout << "operation not supported!" << std::endl;
     }
-    
 }
 
-// int parseImmediate(std::string line){
-//     std::string value = line.substr(3); 
-//     std::cout << value << std::endl;
-//     uint32_t immediateValue;
-//     std::stringstream ss(value);
-//     ss >> std::hex >> immediateValue;
-//     std::cout << immediateValue << std::endl;
-//     return 1000000;
-// }
+uint32_t parseImmediate(std::string line){
+    std::string temp = line.substr(3);
+    std::stringstream ss;
+    uint32_t hexValue;
+    ss << std::hex << temp;
+    ss >> hexValue;
+    return hexValue;
+}
 
-void updateFlags(){
+void updateFlags(std::string instructions[], bool update){
 
 }
